a0caab871c30aedeca806a84a7c09ffc704a430f
chuanqizhu <chuanqizhu1991@outlook.com>
2021-07-06 11:42:29 +0800

    CRUD- create- complete

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,6 +1,16 @@
 class FormsController < ApplicationController
     def new
+        @form = Form.new
     end
     def create
+        @form = Form.new(form_params)
+        if @form.save
+            redirect_to forms_new_path
+        else
+            render :new
+        end
+    end
+    def form_params
+        params.require(:form).permit(:username, :email,:password)
     end
 end
